fc8a45786a4571e65f5d3d7eddf41a09b75b2387
fracs31 <pist.fracs95@gmail.com>
2023-02-08 09:54:28 +0100

    FIX code by deleting the index for the input and using the push()

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 let numbers = []; //numeri che l'utente inserisce
 let N = 10; //lunghezza dell'array di numeri
-let i = 0; //indice
 
 //Fino a quando l'array non è pieno
 while (numbers.length < N) {
@@ -8,13 +7,12 @@ while (numbers.length < N) {
     console.log("Numero inserito dall'utente: " + input); //stampa in console del numero inserito dall'utente
     //Se il numero inserito è dispari
     if ((input % 2) == 1) {
-        numbers[i] = input; //salvo il numero dispari dentro l'array
-        console.log("Numero dispari inserito nell'array: " + numbers[i]); //stampa del numero dispari inserito nell'array
-        i++; //incremento l'indice
+        numbers.push(input); //salvo il numero dispari dentro l'array
+        console.log("Numero inserito: " + numbers[(numbers.length - 1)]);
     }
 }
 
 //Ciclo di stampa
-for (i = 0; i < N; i++) {
+for (let i = 0; i < N; i++) {
     console.log("Numero: " + numbers[i]); //stampo i numeri dentro l'array
 }
